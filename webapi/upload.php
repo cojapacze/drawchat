c0576654fc5a9f07c8e08493feadda46181b7ef6
cojapacze <lukasik.krzysztof@gmail.com>
2024-02-13 10:16:25 +0100

    Add an example of sketchboard video recording with voice from the microphone

--- webapi/upload.php
+++ webapi/upload.php
@@ -3,3 +3,6 @@
 if (@$_FILES['screenshot']) {
   move_uploaded_file($_FILES['screenshot']['tmp_name'], dirname(__FILE__) . '/last_screenshot.jpeg');
 }
+if (@$_FILES['video']) {
+  move_uploaded_file($_FILES['video']['tmp_name'], dirname(__FILE__) . '/last_video.webm');
+}
